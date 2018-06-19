df0bb2c352b468c143264b325c0596a0451da647
liangpeili <liangpeili@foxmail.com>
2018-06-19 11:27:19 +0800

    update index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: ['airbnb-base'],
+  "rules": {
+    "no-console": "off",
+    "no-alert": "off",
+    "no-plusplus": "off",
+    "default-case": "off",
+    "no-param-reassign": [
+      "error",
+      {
+        "props": false
+      }
+    ]
+  }
+}
